2c86741b90177c3450b8d5a40ddf4031dd528687
GuiBrum22 <guilhermegamabrum@gmail.com>
2024-06-11 16:56:35 -0300

    ok

--- 30.04.2024/VScode3/locadora/src/main/java/webapp/locadora/Repository/FeedbackRepository.java
+++
@@ -1,9 +0,0 @@
-package webapp.locadora.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import webapp.locadora.Model.Feedback;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Integer> {
-    
-}
--- 30.04.2024/VScode3/locadora/src/main/java/webapp/locadora/Repository/PertenceRepository.java
+++
@@ -1,9 +0,0 @@
-package webapp.locadora.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import webapp.locadora.Model.Pertence;
-
-public interface PertenceRepository extends JpaRepository<Pertence, Integer> {
-    
-}
